5dba83f5d2b6214ef6379939f23e6c8f3fbb9422
Beasley <beaslera83@gmail.com>
2018-06-22 22:07:23 -0500

    setup script for database

---
+++ Cape_Senior_Center_Inventory_System/setup script.sql
@@ -0,0 +1,420 @@
+﻿
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Beef',       -- CategoryName - nvarchar(max)
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Chicken',       -- CategoryName - nvarchar(max)
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Fish',       -- CategoryName - nvarchar(max)
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Pork',       -- CategoryName - nvarchar(max
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Turkey',       -- CategoryName - nvarchar(max
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Eggs',       -- CategoryName - nvarchar(max
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Other Entrees',       -- CategoryName - nvarchar(max)
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Breads',       -- CategoryName - nvarchar(max)
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Desserts',       -- CategoryName - nvarchar(max)
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Other',       -- CategoryName - nvarchar(max
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Vegetables',       -- CategoryName - nvarchar(max)
+	1,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Dairy',       -- CategoryName - nvarchar(max)
+	2,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Soup Bases',       -- CategoryName - nvarchar(max)
+	2,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Produce',       -- CategoryName - nvarchar(max)
+	2,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Canned',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Meats',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Gallons',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Baking Items',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Baking Mixes',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Jello',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Pudding/Pie Mixes',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Gravy Mixes',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Chips',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Other Dry Items',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'Pasta',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+INSERT INTO dbo.Categories
+(
+    CategoryName,
+    Weight,
+    Created_TS,
+    Updated_TS
+)
+VALUES
+(   'PC Condiments',       -- CategoryName - nvarchar(max)
+	3,
+    GETDATE(), -- Created_TS - datetime
+    GETDATE()  -- Updated_TS - datetime
+)
+
+
+
+
+
+
+SELECT * FROM dbo.Categories
+
+INSERT INTO dbo.UnitOfMeasures
+(
+    Unit
+)
+VALUES ('lbs' -- Unit - nvarchar(max)
+       )
+
+INSERT INTO dbo.UnitOfMeasures
+(
+    Unit
+)
+VALUES ('oz' -- Unit - nvarchar(max)
+       )
+	   INSERT INTO dbo.UnitOfMeasures
+(
+    Unit
+)
+VALUES ('ea' -- Unit - nvarchar(max)
+       )
+	   INSERT INTO dbo.UnitOfMeasures
+(
+    Unit
+)
+VALUES ('ct' -- Unit - nvarchar(max)
+       )
+	   INSERT INTO dbo.UnitOfMeasures
+(
+    Unit
+)
+VALUES ('lbs' -- Unit - nvarchar(max)
+       )
