30ebc869e0edb053de1ae754d19c707b6f25ecf9
Christopher Dunning <christopher.dunning@vizientinc.com>
2018-09-12 16:38:20 -0500

    setup MongoDB Collections when app starts

--- Cape_Senior_Center_Inventory_System/Cape_Senior_Center_Inventory_System/Program.cs
+++ Cape_Senior_Center_Inventory_System/Cape_Senior_Center_Inventory_System/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Cape_Senior_Center_Inventory_System.Interfaces;
 using Castle.Windsor;
 using Castle.Windsor.Installer;
+using MongoDB.Driver;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +19,27 @@ namespace Cape_Senior_Center_Inventory_System
         [STAThread]
         static void Main()
         {
+            MainAsync().Wait();
             IWindsorContainer container = new WindsorContainer();
             //register concrete implementations for container
             container.Install(FromAssembly.This());
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow(container.Resolve<IController>()));
+           }
+
+        static async Task MainAsync()
+        {
+            var client = new MongoClient();
+            var db = client.GetDatabase("VizOnWheels");
+            string[] collectionNames = { "Category", "Inventory", "Item", "ItemCategory", "Price", "UnitOfMeasure" };
+
+            foreach(string collectionName in collectionNames) {
+                if (!db.ListCollectionNames().ToList<string>().Contains(collectionName))
+                {
+                    db.CreateCollection(collectionName);
+                }
+            }
         }
     }
 
