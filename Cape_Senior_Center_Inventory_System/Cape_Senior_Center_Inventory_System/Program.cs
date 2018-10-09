8f5a3dd5291d4028fc2a0c0b21c41aca315fc811
Brian and Chris <BRIAN.GIUNTA@VIZIENTINC.COM>
2018-10-09 10:00:39 -0500

    Remove remaining MongoDB code from project

--- Cape_Senior_Center_Inventory_System/Cape_Senior_Center_Inventory_System/Program.cs
+++ Cape_Senior_Center_Inventory_System/Cape_Senior_Center_Inventory_System/Program.cs
@@ -13,33 +13,15 @@ namespace Cape_Senior_Center_Inventory_System
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            MainAsync().Wait();
             IWindsorContainer container = new WindsorContainer();
             //register concrete implementations for container
             container.Install(FromAssembly.This());
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow(container.Resolve<IController>()));
-           }
-
-        static async Task MainAsync()
-        {
-            var client = new MongoClient();
-            var db = client.GetDatabase("VizOnWheels");
-            string[] collectionNames = { "Category", "Inventory", "Item", "ItemCategory", "Price", "UnitOfMeasure" };
-
-            foreach(string collectionName in collectionNames) {
-                if (!db.ListCollectionNames().ToList<string>().Contains(collectionName))
-                {
-                    db.CreateCollection(collectionName);
-                }
-            }
         }
     }
 
